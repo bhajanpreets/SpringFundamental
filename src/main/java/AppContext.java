0993003fd37128d2b9c2ecac5379f41c6acf95c6
Bhajanpreet Singh <bhajanpreets@gmail.com>
2017-03-28 19:39:07 -0400

    Fully autowird with java config

--- src/main/java/AppContext.java
+++ src/main/java/AppContext.java
@@ -1,22 +1,9 @@
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.bhajan.repository.EmployeeRepositoryStub;
-import com.bhajan.repository.EmployeeRepositoryStubImpl;
-import com.bhajan.service.EmployeeService;
-import com.bhajan.service.EmployeeServiceImpl;
-
 @Configuration
+@ComponentScan("com.bhajan")
 public class AppContext {
-	
-	@Bean(name="employeeService")
-	public EmployeeService getEmployeeService(){
-		return new EmployeeServiceImpl(getEmployeeRepositoryStub());
-	}
-	
-	@Bean(name="employeeRepositoryStub")
-	EmployeeRepositoryStub getEmployeeRepositoryStub(){
-		return new EmployeeRepositoryStubImpl();
-	}
+
 
 }
